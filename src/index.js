1dbb522b0fc54399b3a6259dca74676d2ae10326
Alona Varshal <thirdwave72@gmail.com>
2018-03-13 15:26:35 -0500

    Create markers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 const mapboxgl = require("mapbox-gl");
+const marker = require('./marker')
 
-mapboxgl.accessToken = "pk.eyJ1IjoidGticm9va3MiLCJhIjoiY2plcTI3czNzNXV1cTJ3bG43YTJpMzc3eiJ9.VQWLgG9x7CFu0JCnZHBY5Q";
+// mapboxgl.accessToken = "pk.eyJ1IjoidGticm9va3MiLCJhIjoiY2plcTI3czNzNXV1cTJ3bG43YTJpMzc3eiJ9.VQWLgG9x7CFu0JCnZHBY5Q";
 
 const map = new mapboxgl.Map({
   container: "map",
   center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
-  zoom: 12, // starting zoom
+  zoom: 3, // starting zoom
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
+
+// const marker2Chi = new mapboxgl.Marker()
+// .setLngLat([-87.6354, 41.8885])
+// .addTo(map);
+
+// const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+// markerDomEl.style.width = "32px";
+// markerDomEl.style.height = "39px";
+// markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+// new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map); // [-87.6354, 41.8885] for Chicago
+
+
+
+marker('hotel', -74.009, 40.705).addTo(map); // NY
+marker('restaurant', -87.6354, 41.8885).addTo(map); // chi
---
+++ src/marker.js
@@ -0,0 +1,19 @@
+const mapboxgl = require("mapbox-gl");
+
+
+mapboxgl.accessToken = "pk.eyJ1IjoidGticm9va3MiLCJhIjoiY2plcTI3czNzNXV1cTJ3bG43YTJpMzc3eiJ9.VQWLgG9x7CFu0JCnZHBY5Q";
+const marker = (activity, lat, long) => {
+    const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+    markerDomEl.style.width = "32px";
+    markerDomEl.style.height = "39px";
+    if (activity === 'hotel') {
+        markerDomEl.style.backgroundImage = "url(http://i.imgur.com/D9574Cu.png)";
+    } else if (activity === 'restaurant') {
+        markerDomEl.style.backgroundImage = "url(http://i.imgur.com/cqR6pUI.png)";
+    } else {
+        markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+    }
+    return new mapboxgl.Marker(markerDomEl).setLngLat([lat, long]); 
+}
+
+module.exports = marker;
